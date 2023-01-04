2a0058c34b896a7958dac59b8e69289a80c27ee0
Jeff Nelson <jpnelson85@gmail.com>
2023-01-04 16:20:11 -0600

    add methods

--- array.rb
+++ array.rb
@@ -6,3 +6,30 @@ ticket_prices = [40, 50, 60, 100]
 bank_account = [150.55, 130.34, 24.56, 1099.44]
 
 asleep = [true, true, false, false]
+
+
+
+# Pop       This method grabs the last element in the array.
+puts friends.pop
+puts ticket_prices.pop
+puts bank_account.pop
+puts asleep.pop
+
+# Push      This method takes out the last element in the array.
+puts friends.push
+puts ticket_prices.push
+puts bank_account.push
+puts asleep.push
+
+# shift     This method grabs the first element in the array.
+puts friends.shift
+puts ticket_prices.shift
+puts bank_account.shift
+puts asleep.shift
+
+# unshift    This method takes out the first element in the array and shows the rest.
+puts friends.unshift
+puts ticket_prices.unshift
+puts bank_account.unshift
+puts asleep.unshift
+
