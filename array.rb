ba66b0bc75f7b45b65ec56fe3d88944c39241e9c
Jeff Nelson <jpnelson85@gmail.com>
2023-01-04 16:28:59 -0600

    add sort method definition

--- array.rb
+++ array.rb
@@ -9,29 +9,29 @@ asleep = [true, true, false, false]
 
 
 
-# Pop       This method grabs the last element in the array.
-puts friends.pop
-puts ticket_prices.pop
-puts bank_account.pop
-puts asleep.pop
+# # Pop       This method grabs the last element in the array.
+# puts friends.pop
+# puts ticket_prices.pop
+# puts bank_account.pop
+# puts asleep.pop
 
-# Push      This method takes out the last element in the array.
-puts friends.push
-puts ticket_prices.push
-puts bank_account.push
-puts asleep.push
+# # Push      This method takes out the last element in the array.
+# puts friends.push
+# puts ticket_prices.push
+# puts bank_account.push
+# puts asleep.push
 
-# shift     This method grabs the first element in the array.
-puts friends.shift
-puts ticket_prices.shift
-puts bank_account.shift
-puts asleep.shift
+# # shift     This method grabs the first element in the array.
+# puts friends.shift
+# puts ticket_prices.shift
+# puts bank_account.shift
+# puts asleep.shift
 
-# unshift    This method takes out the first element in the array and shows the rest.
-puts friends.unshift
-puts ticket_prices.unshift
-puts bank_account.unshift
-puts asleep.unshift
+# # unshift    This method takes out the first element in the array and shows the rest.
+# puts friends.unshift
+# puts ticket_prices.unshift
+# puts bank_account.unshift
+# puts asleep.unshift
 
 # The index positions start with 0 and go up from there.  If you look above at the variable titled friends, the index numbers are the following:
 # iggy 0
@@ -39,3 +39,6 @@ puts asleep.unshift
 # mason 2
 # bill 3
 
+puts friends.sort
+
+# With the above array, it sorts the elements based on alphabetically.
