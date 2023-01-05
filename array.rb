3241a9b7a1a50586b84bd1c278e900349676788e
Jeff Nelson <jpnelson85@gmail.com>
2023-01-05 15:26:37 -0600

    update array

--- array.rb
+++ array.rb
@@ -9,25 +9,25 @@ asleep = [true, true, false, false]
 
 
 
-# # Pop       This method grabs the last element in the array.
+# # Pop       This method removes the last element in the array and returns the removed elements.
 # puts friends.pop
 # puts ticket_prices.pop
 # puts bank_account.pop
 # puts asleep.pop
 
-# # Push      This method takes out the last element in the array.
+# # Push      This method pushes the given element at the end of the array and returns the array itself.
 # puts friends.push
 # puts ticket_prices.push
 # puts bank_account.push
 # puts asleep.push
 
-# # shift     This method grabs the first element in the array.
+# # shift     This method removes the first element of the array and returns it.
 # puts friends.shift
 # puts ticket_prices.shift
 # puts bank_account.shift
 # puts asleep.shift
 
-# # unshift    This method takes out the first element in the array and shows the rest.
+# # unshift    This method prepends objects to the front of the array.
 # puts friends.unshift
 # puts ticket_prices.unshift
 # puts bank_account.unshift
